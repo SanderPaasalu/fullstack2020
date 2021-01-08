661e177e849a4ca3efe78c4c27e94a2a6b8779b3
Sander <sander.paasalu@khk.ee>
2021-01-08 12:21:33 +0200

    1.1 again maybe done right this time

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  // const-definitions
-
-  return (
-    <div>
-      <Header course={Fullstack2020} />
-      <Content part={parts}/>
-      <Total exercises={exercises}/>
-    </div>
-  )
-}
-
-const App = () => {
+const App = (Total, Content, Header) => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -22,29 +10,11 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-  return (
+  return (    
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-}
-
-const Content = (parts) => {
-  return (
-    <div>
-      <Part part1={part1} exercises1={exercises1}/>
-      <Part part2={part2} exercises2={exercises2}/>
-      <Part part3={part3} exercises3={exercises3}/>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total exercises={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
