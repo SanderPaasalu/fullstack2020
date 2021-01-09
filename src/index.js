dc20f2fc2580fa163cf0e5c68b257abe3cf6b613
Sander <sander.paasalu@khk.ee>
2021-01-09 10:19:14 +0200

    1.4: course information step4

--- src/index.js
+++ src/index.js
@@ -2,24 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-    const part2 = {
-      name: 'Using props to pass data',
-      exercises: 7
-    }
-    const part3 = {
-      name: 'State of a component',
-      exercises: 14
-    }
   return (    
     <div>
       <Header course={course} />
-      <Content part1 part2 part3/>
-      <Total exercises/>
+      <Content parts={course}/>
+      <Total parts={course}/>
     </div>
   )
 }
