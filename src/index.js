067027d1c0ad836facb0b36efd6188b93d34a9bd
Sander <sander.paasalu@khk.ee>
2021-01-09 10:43:43 +0200

    1.6: unicafe step1

--- src/index.js
+++ src/index.js
@@ -1,54 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-  
-  return (    
-    <div>
-      <Header course={course} />
-      <Content parts={course}/>
-      <Total parts={course}/>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Part part1={part1} exercises1={exercises1}/>
-      <Part part2={part2} exercises2={exercises2}/>
-      <Part part3={part3} exercises3={exercises3}/>
+      code here
     </div>
   )
 }
 
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
