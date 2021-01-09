9dc4ebfa69d3a1907df59ed4ba3e8a7d6b3739b5
Sander <sander.paasalu@khk.ee>
2021-01-09 11:28:42 +0200

    1.5 finished and working now

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <form><button type='button' onClick={() => setGood(good+1)}>good</button>
+      <button type='button' onClick={() => setNeutral(neutral+1)}>neutral</button>
+      <button type='button' onClick={() => setBad(bad+1)}>bad</button>
+      </form>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
