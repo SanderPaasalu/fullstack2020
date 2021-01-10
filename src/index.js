8b58675bb82815deaa704a8ade284a733a7774d7
Sander <sander.paasalu@khk.ee>
2021-01-10 11:36:30 +0200

    1.9: unicafe step4, no feedback to give

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const statistics = (good, neutral, bad) => {
+  if (good>=1 || neutral>=1 || bad>=1)
     return (
       <div>
         <h1>Statistics</h1>
@@ -9,9 +10,12 @@ const statistics = (good, neutral, bad) => {
         <p>Neutral {neutral}</p>  
         <p>Bad {bad}</p>  
         <p>All {good+neutral+bad}</p>  
-        <p>Average {(good*1-bad*1+neutral*0)/8}</p>  
+        <p>Average {(good*1-bad*1+neutral*0)/2}</p>  
         <p>Positive {(good/(good+neutral+bad))*100}</p>   
       </div>
+  )
+  return (
+    <div>No feedback to give</div>  
     )
   }
 
@@ -20,15 +24,17 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
   return (
+    
     <div>
       <h1>give feedback</h1>
       <form><button type='button' onClick={() => setGood(good+1)}>good</button>
       <button type='button' onClick={() => setNeutral(neutral+1)}>neutral</button>
       <button type='button' onClick={() => setBad(bad+1)}>bad</button>
-      </form>
-      {statistics(good, neutral, bad)} 
-    </div>    
+      </form>{statistics(good, neutral, bad)}
+    </div>
+    
   )
 }
 
