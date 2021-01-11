3d349ab881ad58d28beabae38d3849e58249babd
Sander <sander.paasalu@khk.ee>
2021-01-11 10:16:45 +0200

    Working 1.1

--- src/index.js
+++ src/index.js
@@ -1,66 +1,46 @@
-import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const statistics = (good, neutral, bad) => {
-  if (good>=1 || neutral>=1 || bad>=1)
-    return (
-      <div>
-        <th><h1>Statistics</h1></th>
-        <tr><p>Good {good}</p></tr>
-        <tr><p>Neutral {neutral}</p></tr>
-        <tr><p>Bad {bad}</p></tr>  
-        <tr><p>All {good+neutral+bad}</p></tr>  
-        <tr><p>Average {(good*1-bad*1+neutral*0)/2}</p></tr>  
-        <tr><p>Positive {(good/(good+neutral+bad))*100}</p></tr>   
-      </div>
-  )
+const Header = (props) => {
   return (
-    <div>No feedback to give</div>  
-    )
-  }
-
-const Button = (props) => {
-  return (
-    <button type='button' onClick={props.onClick}>{props.text}</button>
-  )
-}
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const handleButtonClick = (type) => {
-    switch(type) {
-      case 'good':
-        setGood(good+1);
-        break;
-      case 'neutral':
-        setNeutral(neutral+1);
-        break;
-      case 'bad':
-        setBad(bad+1);
-        break;
-      default:
-        break;
-    }
-      
-  }
-  return (
-    
     <div>
-      <h1>give feedback</h1>
-      <form>
-      <Button onClick={() => handleButtonClick("good")} text="good" />
-      <Button onClick={() => handleButtonClick("neutral")} text="neutral" />
-      <Button onClick={() => handleButtonClick("bad")} text="bad" />
-      </form>{statistics(good, neutral, bad)}
+    <h1>{props.course}</h1>
     </div>
-    
   )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+const Content = (props) => {
+  return (
+    <div>
+    <p>{props.part1} {props.exercises1}</p>
+    <p>{props.part2} {props.exercises2}</p>
+    <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.exercises1+props.exercises2+props.exercises3}
+    </p>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
